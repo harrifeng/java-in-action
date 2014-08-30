62f1bb03160eb2489adcca6a76c689530988e835
hfeng <haoran.feng@sap.com>
2014-08-30 16:50:01 +0800

    before string.format()

--- src/main/java/org/hfeng/tij4/strings/ArrayListDisplay.java
+++ src/main/java/org/hfeng/tij4/strings/ArrayListDisplay.java
@@ -15,3 +15,10 @@ public class ArrayListDisplay {
         System.out.println(coffees);
     }
 }
+
+/////////////////////////////////////////////////////////////////
+// <===================OUTPUT===================>              //
+// [Americano 0, Latte 1, Americano 2, Mocha 3, Mocha 4,       //
+//  Breve 5, Americano 6, Latte 7, Cappuccino 8, Cappuccino 9] //
+/////////////////////////////////////////////////////////////////
+    
