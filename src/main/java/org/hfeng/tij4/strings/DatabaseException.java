c70305a5fee23865e4b4e204bc5df4dceffb7a97
hfeng <haoran.feng@sap.com>
2014-08-30 16:55:18 +0800

    Before regular expressiongs

---
+++ src/main/java/org/hfeng/tij4/strings/DatabaseException.java
@@ -0,0 +1,21 @@
+package org.hfeng.tij4.strings;
+
+public class DatabaseException extends Exception {
+    public DatabaseException(int transcationID, int queryID,
+                             String message) {
+        super(String.format("(t%d, q%d) %s", transcationID, queryID, message));
+    }
+
+    public static void main(String[] args) {
+        try {
+            throw new DatabaseException(3, 7, "Write failed");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////
+// <===================OUTPUT===================>                      //
+// org.hfeng.tij4.strings.DatabaseException: (t3, q7) Write failed     //
+/////////////////////////////////////////////////////////////////////////
