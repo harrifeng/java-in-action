2a7631cdd57ff8205e7febeaa7fdb2a04420544a
hfeng t60p <harrifeng@gmail.com>
2014-09-02 07:41:29 -0700

    try to write longest palidromic substring

---
+++ src/main/java/org/hfeng/oj/leet/longestpalindromicsubstr/Solution.java
@@ -0,0 +1,54 @@
+package org.hfeng.oj.leet.longestpalindromicsubstr
+
+
+public class Solution {
+    public String longestPalindrome(String s) {
+
+        String ns = polishStr(s);
+
+        int[] value = new int[2010];
+
+        int pivotIndex = 0;
+        int pivotValue = 0;
+        // ^ and $ won't count
+        for (int i = 1; i < ns.length() - 1; i++) {
+            int countpartIndex = Math.max(0, 2 * pivotIndex - i);
+            int startValue = 0;
+            if (value[countpartIndex] > pivotValue)  {
+                startValue = pivotValue;
+            } else {
+                startValue = value[countpartIndex];
+            }
+            while (ns.charAt(startValue + 1) == ns.charAt(startValue - 1)) {
+                startValue++;
+            }
+            value[i] = startValue;
+        }
+
+        int maxIndex = 0;
+        for (int i = 1; i < ns.length(); i++) {
+            if (value[i]  > value[i-1]) {
+                maxIndex = i;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = maxIndex - value[maxIndex]; i < maxIndex + value[maxIndex]; i++) {
+            char cs = ns.charAt(i);
+            if (cs != '^' && cs != '$' && cs != '#') {
+                sb.append(cs);
+            }
+        }
+        return sb.toString();
+    }
+
+    private String polishStr(String s) {
+        StringBuilder sb = new StringBuilder("^#");
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            sb.append("#");
+        }
+        sb.append("$");
+        return sb.toString();
+    }
+}    
---
+++ src/main/java/org/hfeng/oj/leet/longestpalindromicsubstr/question.md
@@ -0,0 +1,8 @@
+Longest Palindromic Substring
+----------
++ Given a string S, find the longest palindromic substring in S.
++ You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
++ For example: 
+  > Input: "abcacbe", Output: 5
+  >
+  > Input: "abbbabbb", Output: 7
