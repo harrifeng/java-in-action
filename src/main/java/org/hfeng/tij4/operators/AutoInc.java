ef144c2b5096b82bce3c193018be2f867ec88d7f
Harri Feng <harrifeng@gmail.com>
2014-09-13 21:03:16 +0800

    before Exponential notation

---
+++ src/main/java/org/hfeng/tij4/operators/AutoInc.java
@@ -0,0 +1,27 @@
+package org.hfeng.tij4.operators;
+
+public class AutoInc {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i:" + i);
+        System.out.println("++i:" + ++i);
+        System.out.println("i++:" + i++);
+
+        System.out.println("i:" + i);
+        System.out.println("--i:" + --i);
+        System.out.println("i--:" + i--);
+
+        System.out.println("i:" + i);
+    }
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// i:1                                            //
+// ++i:2                                          //
+// i++:2                                          //
+// i:3                                            //
+// --i:2                                          //
+// i--:2                                          //
+// i:1                                            //
+////////////////////////////////////////////////////
---
+++ src/main/java/org/hfeng/tij4/operators/EqualsMethod.java
@@ -0,0 +1,14 @@
+package org.hfeng.tij4.operators;
+
+public class EqualsMethod {
+    public static void main(String[] args) {
+        Integer n1 = new Integer(47);
+        Integer n2 = new Integer(47);
+        System.out.println(n1.equals(n2));
+    }
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// true                                           //
+////////////////////////////////////////////////////
