8eb467cd7b57c3a203f766a1058571689f64c36a
hfeng <haoran.feng@sap.com>
2014-09-17 23:48:27 -0700

    add two numbers

--- src/main/java/org/hfeng/oj/leet/addtwonumbers/Solution.java
+++ src/main/java/org/hfeng/oj/leet/addtwonumbers/Solution.java
@@ -6,21 +6,23 @@ public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int adv = 0;
         ListNode head = new ListNode(-1);
-        ListNode ret = head;
+        ListNode tmp = head;
         while (l1 != null || l2 != null) {
             int v1 = (l1 == null) ? 0 : l1.val;
             int v2 = (l2 == null) ? 0 : l2.val;
+
             int sum = v1 + v2 + adv;
+            adv = sum / 10;
             head.next = new ListNode(sum % 10);
             head = head.next;
-            adv = sum / 10;
 
             l1 = (l1 == null) ? null : l1.next;
             l2 = (l2 == null) ? null : l2.next;
         }
+
         if (adv > 0) {
             head.next = new ListNode(1);
         }
-        return ret.next;
+        return tmp.next;
     }
 }
