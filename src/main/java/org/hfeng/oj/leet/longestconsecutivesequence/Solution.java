a09cd4aec14439f7571823df33cdbddac78fe8de
hfeng <haoran.feng@sap.com>
2014-09-18 23:52:41 -0700

    add consecutive by myself

--- src/main/java/org/hfeng/oj/leet/longestconsecutivesequence/Solution.java
+++ src/main/java/org/hfeng/oj/leet/longestconsecutivesequence/Solution.java
@@ -1,38 +1,34 @@
 package org.hfeng.oj.leet.longestconsecutivesequence;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
     public int longestConsecutive(int[] num) {
-        if (num.length == 0) {
-            return 0;
-        }
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        int len = 1;
+        int maxLen = 1;
 
         for (int i = 0; i < num.length; i++) {
-            if (map.containsKey(num[i])) {
-                continue;
-            }
+            // At least the length is 1
             map.put(num[i], 1);
-            if (map.containsKey(num[i] - 1)) {
-                len = Math.max(len, mergeCluster(map, num[i] - 1, num[i]));
-            }
-            if (map.containsKey(num[i] + 1)) {
-                len = Math.max(len, mergeCluster(map, num[i], num[i] + 1));
-            }
+            maxLen = Math.max(maxLen, findNearby(map, num[i]));
         }
-        return len;
+        return maxLen;
     }
 
-    private int mergeCluster(Map<Integer, Integer> map, int left, int right) {
-        int upper = right + map.get(right) - 1;
-        int lower = left - map.get(left) + 1;
-        int length = upper - lower + 1;
-        map.put(upper, length);
-        map.put(lower, length);
-        return length;
+    private int findNearby(Map<Integer, Integer> map, int val) {
+        if (!map.containsKey(val+1) && !map.containsKey(val-1)) {
+            return 1;
+        }
+        int left = 0;
+        int right = 0;
+        if (map.containsKey(val-1)) {
+            left = map.get(val-1);
+        }
+        if (map.containsKey(val+1)) {
+            right = map.get(val+1);
+        }
+        int newLen = left + right + 1;
+        map.put(val, newLen);
+        return newLen;
     }
 }
