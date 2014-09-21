100b9e1ffe592d3ec9d584a58f2758c92d6048a9
Harri Feng <harrifeng@gmail.com>
2014-09-21 11:41:25 +0800

    add new leet

--- src/main/java/org/hfeng/oj/leet/util/Test.java
+++ src/main/java/org/hfeng/oj/leet/util/Test.java
@@ -1,7 +1,16 @@
 package org.hfeng.oj.leet.util;
 
 
+import org.hfeng.tij4.util.CountingGenerator;
 
 public class Test {
+    public static void main(String[] args) {
+        int ii = Character.digit('1', 10);
 
+        System.out.println(ii);
+    }
+
+    public void removeStr(String s) {
+
+    }
 }
