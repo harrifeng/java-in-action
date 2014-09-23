399f7747356ba6b651ed749184871a14e3f1735c
Harri Feng <harrifeng@gmail.com>
2014-09-23 21:33:21 +0800

    finish chapter 5

---
+++ src/main/java/org/hfeng/tij4/initialization/DefaultConstructor.java
@@ -0,0 +1,9 @@
+package org.hfeng.tij4.initialization;
+
+class Bird{}
+
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Bird b = new Bird(); // Default
+    }
+}
---
+++ src/main/java/org/hfeng/tij4/initialization/SimpleEnumUse.java
@@ -0,0 +1,13 @@
+package org.hfeng.tij4.initialization;
+
+public class SimpleEnumUse {
+    public static void main(String[] args) {
+        Spiciness howHot = Spiciness.MEDIUM;
+        System.out.println(howHot);
+    }
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// MEDIUM                                         //
+////////////////////////////////////////////////////
