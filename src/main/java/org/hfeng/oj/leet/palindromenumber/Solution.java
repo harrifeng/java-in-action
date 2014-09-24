b2f1c3fc088f3ecb253ec7652e0a01312bbcc8d7
hfeng <haoran.feng@sap.com>
2014-09-23 23:41:02 -0700

    further understand this question

--- src/main/java/org/hfeng/oj/leet/palindromenumber/Solution.java
+++ src/main/java/org/hfeng/oj/leet/palindromenumber/Solution.java
@@ -10,19 +10,21 @@ public class Solution {
             return true;
         }
 
-        int size = 1;
         int tmp = x;
+        int times = 1;
         while (tmp >= 10) {
-            tmp = tmp / 10;
-            size = size * 10;
+            times *= 10;
+            tmp /= 10;
         }
 
-        while (size >= 10) {
-            if (x / size != x % 10) {
+        // MISTAKE HERE
+        // 009 can be only show 9, while times always has 1 at front
+        while (times >= 10) {
+            if (x / times != x % 10) {
                 return false;
             }
-            x = x % size / 10;
-            size = size / 100;
+            x  = x % times / 10;
+            times /= 100;
         }
         return true;
     }
