e26b93c1776700e9ce2fef434e7aaa2eb2edbc87
Harri Feng <harrifeng@gmail.com>
2014-09-25 20:53:03 +0800

    finish chapter 6

---
+++ src/main/java/org/hfeng/tij4/access/dessert/Cookie.java
@@ -0,0 +1,10 @@
+package org.hfeng.tij4.access.dessert;
+
+public class Cookie {
+    public Cookie() {
+        System.out.printf("Cookie ctor");
+    }
+    void bite() {
+        System.out.printf("bibe");
+    }
+}
