209d5505db946a5555a6f03c98130f3cd8cadacf
Harri Feng <harrifeng@gmail.com>
2014-10-08 22:39:10 +0800

    finish chapter 09

---
+++ src/main/java/org/hfeng/tij4/interfaces/filters/LowPass.java
@@ -0,0 +1,11 @@
+package org.hfeng.tij4.interfaces.filters;
+
+public class LowPass extends Filter {
+    double cutoff;
+    public LowPass(double cutoff) {
+        this.cutoff = cutoff;
+    }
+    public Waveform process(Waveform input) {
+        return input; // Dummy processing
+    }
+}
