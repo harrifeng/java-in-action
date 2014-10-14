b9e6020f34fd2a3fe390533c4acdb68dabb4864a
hfeng <haoran.feng@sap.com>
2014-10-13 21:53:29 -0700

    bright solution from practice branch

--- src/main/java/org/hfeng/oj/leet/regularexpressionmatching/Solution.java
+++ src/main/java/org/hfeng/oj/leet/regularexpressionmatching/Solution.java
@@ -2,39 +2,27 @@ package org.hfeng.oj.leet.regularexpressionmatching;
 
 public class Solution {
     public boolean isMatch(String s, String p) {
-        // recursive method should find way to exit FIRSTLY!!
         if (p.length() == 0) {
             return s.length() == 0;
         }
 
-        // p == 1 is a special case, should be consider alone
+        ////////////////////////////////////////////////////////////////////////////////
+        // `if (p.length() == 1)`  and `if (p.charAt(1) != '*')`  use the SAME action //
+        ////////////////////////////////////////////////////////////////////////////////
         if (p.length() == 1) {
-            return (s.length() == 1)
-                    && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0));
+            return s.length() == 1 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0));
         }
 
-        // In following cases, p.length() should always be greator than 1,
-        // so no overflow should be considered.
-
-        // Next p is not *
+        //p  must be bigger than 1 here
         if (p.charAt(1) != '*') {
-            // Previous judgement can only make sure p is greater than 1,
-            // so we must make sure p is greater or equal to 1 otherwise,
-            // s.substring(1) can not work
-            return (s.length() >= 1
-                    && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')
-                    && isMatch(s.substring(1), p.substring(1)));
-        }
-
-        // Next p does be *
-        if (isMatch(s, p.substring(2))) {
-            // try to match whole string FIRST!!
-            // becasue `x*` can match ZERO character!!
-            return true;
+            return s.length() > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
+                && isMatch(s.substring(1), p.substring(1));
         } else {
-            return (s.length() >= 1
-                    && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')
-                    && isMatch(s.substring(1), p));
+            if (isMatch(s, p.substring(2))) {
+                return true;
+            }
+            return s.length() > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
+                && isMatch(s.substring(1), p);
         }
     }
 }
