2f94d09948dfcd6f97a72f3469690a27a389831a
hfeng <haoran.feng@sap.com>
2014-10-19 23:07:51 -0700

    first today

--- src/main/java/org/hfeng/oj/leet/addtwonumbers/Solution.java
+++ src/main/java/org/hfeng/oj/leet/addtwonumbers/Solution.java
@@ -10,9 +10,9 @@ public class Solution {
         while (l1 != null || l2 != null) {
             int v1 = (l1 == null) ? 0 : l1.val;
             int v2 = (l2 == null) ? 0 : l2.val;
-
             int sum = v1 + v2 + adv;
             adv = sum / 10;
+
             head.next = new ListNode(sum % 10);
             head = head.next;
 
