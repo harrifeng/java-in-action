3d4329c98645f4519ea38baf4798a1e1044f7379
hfeng <haoran.feng@sap.com>
2014-10-19 23:27:34 -0700

    today second

--- src/main/java/org/hfeng/oj/leet/atoi/Solution.java
+++ src/main/java/org/hfeng/oj/leet/atoi/Solution.java
@@ -5,35 +5,33 @@ public class Solution {
         if (str.length() == 0) {
             return 0;
         }
-        int start = 0;
-        while (str.charAt(start) == ' ') {
-            start++;
+
+        int beg = 0;
+        while (str.charAt(beg) == ' ') {
+            beg++;
         }
 
-        boolean negative = false;
-
-        if (str.charAt(start) == '-') {
-            negative = true;
-            start++;
-        } else if (str.charAt(start) == '+') {
-            start++;
+        boolean neg = false;
+        if (str.charAt(beg) == '-') {
+            beg++;
+            neg = true;
+        } else if (str.charAt(beg) == '+') {
+            beg++;
         }
 
         int ret = 0;
-        for (int i = start; i < str.length(); i++) {
-            // USEFUL HERE
+        for (int i = beg; i < str.length(); i++) {
             if (Character.isDigit(str.charAt(i))) {
                 if (ret > Integer.MAX_VALUE / 10
-                        || (ret == Integer.MAX_VALUE / 10
-                        && Character.digit(str.charAt(i), 10) > Integer.MAX_VALUE % 10)) {
-                    return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                    || (ret == Integer.MAX_VALUE/ 10 && Character.digit(str.charAt(i), 10) > Integer.MAX_VALUE % 10)) {
+                    return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
                 }
-                // USEFUL HERE
                 ret = ret * 10 + Character.digit(str.charAt(i), 10);
             } else {
                 break;
             }
+
         }
-        return negative ? (-1) * ret : ret;
+        return neg ? (-1) * ret : ret;
     }
 }
