55dc1040d1bc08e76694a4f6e0054d5667438cba
hfeng <haoran.feng@sap.com>
2014-10-20 22:54:15 -0700

    first today

--- src/main/java/org/hfeng/oj/leet/foursum/Solution.java
+++ src/main/java/org/hfeng/oj/leet/foursum/Solution.java
@@ -4,32 +4,33 @@ import java.util.*;
 
 public class Solution {
     public List<List<Integer>> fourSum(int[] num, int target) {
+        if (num.length < 4) {
+            return null;
+        }
+
         List<List<Integer>> ret = new ArrayList<List<Integer>>();
 
         Arrays.sort(num);
-        // Assume num.length is bigger than or equal to 4
-        for (int i = 0; i < num.length - 2; i++) {
-            if (i > 0 && num[i] == num[i - 1]) {
+        for (int i = 0; i < num.length - 3; i++) {
+            if (i > 0 && num[i] == num[i-1]) {
                 continue;
             }
-            // MISTAKE HERE
-            for (int j = i + 1; j < num.length - 1; j++) {
-                if (j > i + 1 && num[j] == num[j - 1]) {
+            for (int j = i + 1; j < num.length - 2; j++) {
+                if (j > i + 1 && num[j] == num[j-1]) {
                     continue;
                 }
-
                 int beg = j + 1;
                 int end = num.length - 1;
                 while (beg < end) {
                     int sum = num[i] + num[j] + num[beg] + num[end];
                     if (sum == target) {
-                        List<Integer> oneRet = new ArrayList<Integer>();
-                        oneRet.add(num[i]);
-                        oneRet.add(num[j]);
-                        oneRet.add(num[beg]);
-                        oneRet.add(num[end]);
-                        ret.add(oneRet);
-                        while (beg < end && num[beg] == num[beg + 1] && num[end] == num[end - 1]) {
+                        ArrayList<Integer> one = new ArrayList<Integer>();
+                        one.add(num[i]);
+                        one.add(num[j]);
+                        one.add(num[beg]);
+                        one.add(num[end]);
+                        ret.add(one);
+                        while (beg < end && num[beg+1] == num[beg] && num[end-1] == num[end]) {
                             beg++;
                             end--;
                         }
