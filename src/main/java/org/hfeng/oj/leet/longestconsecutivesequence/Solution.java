2aee735e957b899b21bf7e928bb5f74a0e4d56bc
hfeng <haoran.feng@sap.com>
2014-10-21 01:07:42 -0700

    second today, be sure to try this online

--- src/main/java/org/hfeng/oj/leet/longestconsecutivesequence/Solution.java
+++ src/main/java/org/hfeng/oj/leet/longestconsecutivesequence/Solution.java
@@ -1,34 +1,21 @@
 package org.hfeng.oj.leet.longestconsecutivesequence;
-
 import java.util.*;
 
 public class Solution {
     public int longestConsecutive(int[] num) {
+        int maxSize = 1;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int maxLen = 1;
-
         for (int i = 0; i < num.length; i++) {
-            // At least the length is 1
-            map.put(num[i], 1);
-            maxLen = Math.max(maxLen, findNearby(map, num[i]));
+            int size = 1;
+            if (map.containsKey(num[i]-1)) {
+                size += map.get(num[i]-1);
+            }
+            if (map.containsKey(num[i]+1)) {
+                size += map.get(num[i]+1);
+            }
+            maxSize = Math.max(maxSize, size);
+            map.put(num[i], size);
         }
-        return maxLen;
-    }
-
-    private int findNearby(Map<Integer, Integer> map, int val) {
-        if (!map.containsKey(val+1) && !map.containsKey(val-1)) {
-            return 1;
-        }
-        int left = 0;
-        int right = 0;
-        if (map.containsKey(val-1)) {
-            left = map.get(val-1);
-        }
-        if (map.containsKey(val+1)) {
-            right = map.get(val+1);
-        }
-        int newLen = left + right + 1;
-        map.put(val, newLen);
-        return newLen;
+        return maxSize;
     }
 }
