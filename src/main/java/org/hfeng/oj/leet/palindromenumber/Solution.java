ba0a8776f5d6e2cc08d13710ad401e309c7a9052
hfeng <haoran.feng@sap.com>
2014-10-23 23:41:32 -0700

    first today, good test case is good way to understand, for example 101001 is such case, so it is with 10091

--- src/main/java/org/hfeng/oj/leet/palindromenumber/Solution.java
+++ src/main/java/org/hfeng/oj/leet/palindromenumber/Solution.java
@@ -1,30 +1,25 @@
 package org.hfeng.oj.leet.palindromenumber;
-
 public class Solution {
     public boolean isPalindrome(int x){
         if (x < 0) {
             return false;
         }
-
         if (x < 10) {
             return true;
         }
-
+        // x is bigger than 9 here
         int tmp = x;
-        int times = 1;
+        int size = 1;
         while (tmp >= 10) {
-            times *= 10;
+            size *= 10;
             tmp /= 10;
         }
-
-        // MISTAKE HERE
-        // 009 can be only show 9, while times always has 1 at front
-        while (times >= 10) {
-            if (x / times != x % 10) {
+        while (size >= 10) {
+            if ( x % 10 != x / size) {
                 return false;
             }
-            x  = x % times / 10;
-            times /= 100;
+            x = x % size / 10;
+            size = size / 100;
         }
         return true;
     }
