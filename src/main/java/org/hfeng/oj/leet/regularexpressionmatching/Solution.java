6c451ce5cdedbc26bc1da5a46ebe92b3e85f5fd1
Harri Feng <harrifeng@gmail.com>
2014-10-27 20:21:27 +0800

    first today, use () even you are sure

--- src/main/java/org/hfeng/oj/leet/regularexpressionmatching/Solution.java
+++ src/main/java/org/hfeng/oj/leet/regularexpressionmatching/Solution.java
@@ -1,28 +1,25 @@
 package org.hfeng.oj.leet.regularexpressionmatching;
-
 public class Solution {
     public boolean isMatch(String s, String p) {
         if (p.length() == 0) {
             return s.length() == 0;
         }
-
-        ////////////////////////////////////////////////////////////////////////////////
-        // `if (p.length() == 1)`  and `if (p.charAt(1) != '*')`  use the SAME action //
-        ////////////////////////////////////////////////////////////////////////////////
         if (p.length() == 1) {
-            return s.length() == 1 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0));
+            return (s.length() == 1
+                    && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)));
         }
-
-        //p  must be bigger than 1 here
+        // p is bigger than 1 here
         if (p.charAt(1) != '*') {
-            return s.length() > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
-                && isMatch(s.substring(1), p.substring(1));
+            return (s.length() >= 1
+                    && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
+                    && isMatch(s.substring(1), p.substring(1)));
         } else {
             if (isMatch(s, p.substring(2))) {
                 return true;
             }
-            return s.length() > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
-                && isMatch(s.substring(1), p);
+            return (s.length() >= 1
+                    && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
+                    && isMatch(s.substring(1), p));
         }
     }
 }
