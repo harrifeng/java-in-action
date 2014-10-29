3efaafbf39e89083701aeba2eb5c72ff04614d57
hfeng <haoran.feng@sap.com>
2014-10-29 02:16:30 -0700

    from practice

--- src/main/java/org/hfeng/oj/leet/removeduplicatefromsortedarraytwo/Solution.java
+++ src/main/java/org/hfeng/oj/leet/removeduplicatefromsortedarraytwo/Solution.java
@@ -2,24 +2,22 @@ package org.hfeng.oj.leet.removeduplicatefromsortedarraytwo;
 
 public class Solution {
     public int removeDuplicates(int[] A) {
-        if (A.length == 0) {
-            return 0;
+        if (A.length <= 2) {
+            return A.length;
         }
-
-        int index = 0;
+        int start = 0;
         int count = 1;
-
         for (int i = 1; i < A.length; i++) {
-            if (A[index] != A[i]) {
-                A[++index] = A[i];
+            if (A[i] != A[i-1]) {
+                A[++start] = A[i];
                 count = 1;
             } else {
                 count++;
                 if (count == 2) {
-                    A[++index] = A[i];
+                    A[++start] = A[i];
                 }
             }
         }
-        return index + 1;
+        return start + 1;
     }
 }
