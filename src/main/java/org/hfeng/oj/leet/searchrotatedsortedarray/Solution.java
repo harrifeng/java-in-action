40b048221e1d549e4beb0731e2ac9214e243e2b5
hfeng <haoran.feng@sap.com>
2014-11-03 01:06:59 -0800

    deep understanding

--- src/main/java/org/hfeng/oj/leet/searchrotatedsortedarray/Solution.java
+++ src/main/java/org/hfeng/oj/leet/searchrotatedsortedarray/Solution.java
@@ -1,8 +1,6 @@
 package org.hfeng.oj.leet.searchrotatedsortedarray;
-
 public class Solution {
     public int search(int[] A, int target) {
-
         int left = 0;
         int right = A.length - 1;
 
@@ -11,21 +9,28 @@ public class Solution {
             if (A[mid] == target) {
                 return mid;
             }
-
+            /////////////////////////////////////////////////////////////////////////////////
+            // value: 45601234                                                             //
+            // index: 01234567                                                             //
+            // if left = 0, mid = 3, then left part is positive sequence, right is not     //
+            // if left = 0, mid = 4, then the right part is positive sequence, left is not //
+            // That is to say: There are at least one part is positive sequence            //
+            /////////////////////////////////////////////////////////////////////////////////
             if (A[left] <= A[mid]) {
-                // left is small than mid
-                // target is between left and mid,
-                // in this case, left part is MUST the solution.
-                // in all other cases, right part is the solution.
                 if (A[left] <= target && target < A[mid]) {
+                    // left part is positive sequence, smaller binary search
                     right = mid;
                 } else {
+                    // right part is Not positive sequence, it is a smaller THIS question
                     left = mid + 1;
                 }
             } else {
+
                 if (A[mid] < target && target <= A[right]) {
+                    // right part is positive sequence, smaller binary search
                     left = mid + 1;
                 } else {
+                    // left part is Not positive sequence, it is a smaller THIT question
                     right = mid;
                 }
             }
