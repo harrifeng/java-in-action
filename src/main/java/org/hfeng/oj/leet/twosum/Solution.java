13693b51dc8f2d3a16c6dfffd08265e212bd2772
hfeng <haoran.feng@sap.com>
2014-11-05 00:12:49 -0800

    at least one solution, so it is also ok in our way

--- src/main/java/org/hfeng/oj/leet/twosum/Solution.java
+++ src/main/java/org/hfeng/oj/leet/twosum/Solution.java
@@ -1,20 +1,20 @@
 package org.hfeng.oj.leet.twosum;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.hfeng.oj.leet.util.*;
+import java.util.*;
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < numbers.length; i++) {
+        int[] ret = new int[2];
+        for(int i = 0; i < numbers.length; i++) {
             if (map.containsKey(target - numbers[i])) {
-                int[] ret = new int[2];
-                ret[0] = Math.min(i + 1, map.get(target - numbers[i]));
-                ret[1] = Math.max(i + 1, map.get(target - numbers[i]));
+                ret[0] = map.get(target - numbers[i]);
+                ret[1] = i + 1;
                 return ret;
             }
             map.put(numbers[i], i + 1);
         }
-        return null;
+        return ret;
     }
 }
