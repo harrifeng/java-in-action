ee56e9a3246830e25a73291266aea127242d3190
Harri Feng <harri.feng@gmail.com>
2014-11-18 19:53:59 -0800

    second

--- src/main/java/org/hfeng/oj/leet/atoi/Solution.java
+++ src/main/java/org/hfeng/oj/leet/atoi/Solution.java
@@ -1,19 +1,16 @@
 package org.hfeng.oj.leet.atoi;
-
 public class Solution {
     public int atoi(String str) {
         if (str.length() == 0) {
             return 0;
         }
         int start = 0;
+        boolean neg = false;
         while (str.charAt(start) == ' ') {
             start++;
         }
-
-        boolean negative = false;
-
         if (str.charAt(start) == '-') {
-            negative = true;
+            neg = true;
             start++;
         } else if (str.charAt(start) == '+') {
             start++;
@@ -21,19 +18,16 @@ public class Solution {
 
         int ret = 0;
         for (int i = start; i < str.length(); i++) {
-            // USEFUL HERE
-            if (Character.isDigit(str.charAt(i))) {
+            char current = str.charAt(i);
+            if (Character.isDigit(current)) {
                 if (ret > Integer.MAX_VALUE / 10
-                        || (ret == Integer.MAX_VALUE / 10
-                        && Character.digit(str.charAt(i), 10) > Integer.MAX_VALUE % 10)) {
-                    return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                    || (ret == Integer.MAX_VALUE / 10
+                        && (current - '0') > Integer.MAX_VALUE % 10)) {
+                    return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
                 }
-                // USEFUL HERE
-                ret = ret * 10 + Character.digit(str.charAt(i), 10);
-            } else {
-                break;
+                ret = ret * 10 + (current - '0');
             }
         }
-        return negative ? (-1) * ret : ret;
+        return neg ? (-1) * ret : ret;
     }
 }
