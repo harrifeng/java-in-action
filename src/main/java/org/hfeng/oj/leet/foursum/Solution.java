7c8633d6568fe3e057ced8b5ea4d09e894b06749
Harri Feng <harri.feng@gmail.com>
2014-11-20 02:54:44 -0800

    3rd

--- src/main/java/org/hfeng/oj/leet/foursum/Solution.java
+++ src/main/java/org/hfeng/oj/leet/foursum/Solution.java
@@ -1,25 +1,23 @@
 package org.hfeng.oj.leet.foursum;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-
+import java.util.*;
 public class Solution {
     public List<List<Integer>> fourSum(int[] num, int target) {
-        List<List<Integer>> ret  = new ArrayList<List<Integer>>();
-
+        if (num.length < 4) {
+            return null;
+        }
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
         Arrays.sort(num);
 
         for (int i = 0; i < num.length - 3; i++) {
-            if (i > 0 && num[i] == num[i - 1]) {
+            if (i > 0 && num[i] == num[i-1]) {
                 continue;
             }
             for (int j = i + 1; j < num.length - 2; j++) {
-                if (j > i + 1 && num[j] == num[j - 1]) {
+                if (j > i + 1 && num[j] == num[j-1]) {
                     continue;
                 }
                 int beg = j + 1;
-                int end = num.length - 1;
+                int end = num.length - 1 ;
                 while (beg < end) {
                     int sum = num[i] + num[j] + num[beg] + num[end];
                     if (sum == target) {
