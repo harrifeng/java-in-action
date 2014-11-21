37212b232a75a7012815f11527084c7f6ac8854f
Harri Feng <harri.feng@gmail.com>
2014-11-20 20:05:02 -0800

    4th

--- src/main/java/org/hfeng/oj/leet/longestconsecutivesequence/Solution.java
+++ src/main/java/org/hfeng/oj/leet/longestconsecutivesequence/Solution.java
@@ -1,38 +1,21 @@
 package org.hfeng.oj.leet.longestconsecutivesequence;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
 public class Solution {
     public int longestConsecutive(int[] num) {
-        if (num.length == 0) {
-            return 0;
-        }
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        int len = 1;
+        Map<Integer, Integer> hash = new HashMap<Integer, Integer>();
+        int maxSize = 0;
 
         for (int i = 0; i < num.length; i++) {
-            if (map.containsKey(num[i])) {
-                continue;
+            int size = 1;
+            if (hash.containsKey(num[i] - 1)) {
+                size += hash.get(num[i] - 1);
             }
-            map.put(num[i], 1);
-            if (map.containsKey(num[i] - 1)) {
-                len = Math.max(len, mergeCluster(map, num[i] - 1, num[i]));
+            if (hash.containsKey(num[i] + 1)) {
+                size += hash.get(num[i] + 1);
             }
-            if (map.containsKey(num[i] + 1)) {
-                len = Math.max(len, mergeCluster(map, num[i], num[i] + 1));
-            }
+            hash.put(num[i], size);
+            maxSize = Math.max(maxSize, size);
         }
-        return len;
-    }
-
-    private int mergeCluster(Map<Integer, Integer> map, int left, int right) {
-        int upper = right + map.get(right) - 1;
-        int lower = left - map.get(left) + 1;
-        int length = upper - lower + 1;
-        map.put(upper, length);
-        map.put(lower, length);
-        return length;
+        return maxSize;
     }
 }
