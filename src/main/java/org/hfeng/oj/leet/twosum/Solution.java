69d600017dc31898fba731df0b16fcc32554104c
Harri Feng <harri.feng@gmail.com>
2014-12-11 23:33:49 -0800

    21th

--- src/main/java/org/hfeng/oj/leet/twosum/Solution.java
+++ src/main/java/org/hfeng/oj/leet/twosum/Solution.java
@@ -1,20 +1,19 @@
 package org.hfeng.oj.leet.twosum;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] ret = new int[2];
         for (int i = 0; i < numbers.length; i++) {
             if (map.containsKey(target - numbers[i])) {
-                int[] ret = new int[2];
-                ret[0] = Math.min(i + 1, map.get(target - numbers[i]));
-                ret[1] = Math.max(i + 1, map.get(target - numbers[i]));
+                ret[0] = map.get(target - numbers[i]);
+                ret[1] = i + 1;
                 return ret;
+            } else {
+                map.put(numbers[i], i + 1);
             }
-            map.put(numbers[i], i + 1);
         }
-        return null;
+        return ret;
     }
 }
