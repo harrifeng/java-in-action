bf0d4e82bccea6cc167810af44f1d8a017b24ea1
Harri Feng <harri.feng@gmail.com>
2014-12-11 22:37:46 -0800

    20th

--- src/main/java/org/hfeng/oj/leet/trappingrainwater/Solution.java
+++ src/main/java/org/hfeng/oj/leet/trappingrainwater/Solution.java
@@ -1,27 +1,37 @@
 package org.hfeng.oj.leet.trappingrainwater;
-
 public class Solution {
     public int trap(int[] A) {
         if (A.length < 2) {
             return 0;
         }
+
         int left = 0;
-        int right = A.length - 1;
         int ret = 0;
-        while (left < right) {
-            int min = Math.min(A[left], A[right]);
-            if (A[left] == min) {
-                left++;
-                while (left < right && A[left] < min) {
-                    ret += min - A[left];
-                    left++;
-                }
+        int sum = 0;
+        int highest = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > A[highest]) {
+                highest = i;
+            }
+        }
+        for (int i = 1; i <= highest; i++) {
+            if (A[left] <= A[i]) {
+                ret += (i - left - 1) * A[left] - sum;
+                sum = 0;
+                left = i;
             } else {
-                right--;
-                while (left < right && A[right] < min) {
-                    ret += min - A[right];
-                    right--;
-                }
+                sum += A[i];
+            }
+        }
+        int right = A.length - 1;
+        sum = 0;
+        for (int i = A.length - 2; i >= highest; i--) {
+            if (A[right] <= A[i]) {
+                ret += (right - i - 1) * A[right] - sum;
+                sum = 0;
+                right = i;
+            } else {
+                sum += A[i];
             }
         }
         return ret;
