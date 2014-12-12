ca4dd698c30172ec6576a0f5f00dc391cada5d5b
Harri Feng <harri.feng@gmail.com>
2014-12-12 01:14:35 -0800

    From p1 branch, as this solution looks much easier

--- src/main/java/org/hfeng/oj/leet/validsudoku/Solution.java
+++ src/main/java/org/hfeng/oj/leet/validsudoku/Solution.java
@@ -1,46 +1,31 @@
 package org.hfeng.oj.leet.validsudoku;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        ArrayList<HashMap<Integer, Integer>> row_list_map = new ArrayList<HashMap<Integer, Integer>>();
-        ArrayList<HashMap<Integer, Integer>> col_list_map = new ArrayList<HashMap<Integer, Integer>>();
-        ArrayList<HashMap<Integer, Integer>> grid_list_map = new ArrayList<HashMap<Integer, Integer>>();
+        List<boolean[]> lineList = new ArrayList<boolean[]>();
+        List<boolean[]> columnList = new ArrayList<boolean[]>();
+        List<boolean[]> squareList = new ArrayList<boolean[]>();
+
         for (int i = 0; i < 9; i++) {
-            HashMap<Integer, Integer> tmp = new HashMap<Integer, Integer>();
-            row_list_map.add(tmp);
-            tmp = new HashMap<Integer, Integer>();
-            col_list_map.add(tmp);
-            tmp = new HashMap<Integer, Integer>();
-            grid_list_map.add(tmp);
+            lineList.add(new boolean[9]);
+            columnList.add(new boolean[9]);
+            squareList.add(new boolean[9]);
         }
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if(board[i][j] == '.') {
                     continue;
                 }
-
-                if (row_list_map.get(i).containsKey(board[i][j] - '0')) {
+                int num = board[i][j] - '1';
+                if(lineList.get(i)[num] || columnList.get(j)[num] || squareList.get(i / 3 * 3 + j/ 3)[num]) {
                     return false;
-                } else {
-                    row_list_map.get(i).put(board[i][j] - '0', 1);
                 }
-
-                if (col_list_map.get(j).containsKey(board[i][j] - '0')) {
-                    return false;
-                } else {
-                    col_list_map.get(j).put(board[i][j] - '0', 1);
-                }
-
-                int index = ((i / 3) * 3) + (j / 3);
-                if (grid_list_map.get(index).containsKey(board[i][j] - '0')) {
-                    return false;
-                } else {
-                    grid_list_map.get(index).put(board[i][j] - '0', 1);
-                }
+                lineList.get(i)[num] = true;
+                columnList.get(j)[num] = true;
+                squareList.get(i / 3 * 3 + j / 3)[num] = true;
             }
         }
         return true;
