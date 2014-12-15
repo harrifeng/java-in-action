c7180d91cc6b20cf93866428f337eceba4eb375d
Harri Feng <harri.feng@gmail.com>
2014-12-14 23:06:25 -0800

    5th

--- src/main/java/org/hfeng/oj/leet/longestsubstringwithoutrepeat/Solution.java
+++ src/main/java/org/hfeng/oj/leet/longestsubstringwithoutrepeat/Solution.java
@@ -1,23 +1,22 @@
 package org.hfeng.oj.leet.longestsubstringwithoutrepeat;
-
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         int[] index = new int[256];
         Arrays.fill(index, -1);
-        int maxLen = 0;
         int len = 0;
+        int max = 0;
         for (int i = 0; i < s.length(); i++) {
             if (index[s.charAt(i)] != -1) {
-                maxLen = Math.max(maxLen, len);
-                len = 0;
+                max = Math.max(max, len);
                 i = index[s.charAt(i)] + 1;
+                len = 0;
                 Arrays.fill(index, -1);
             }
-            len++;
             index[s.charAt(i)] = i;
+            len++;
         }
-        return Math.max(maxLen, len);
+        return Math.max(max, len);
     }
 }
