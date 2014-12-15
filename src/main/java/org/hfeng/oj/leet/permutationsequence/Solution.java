0c1814c9bf0187a6046e47196eec20a08fefe8a3
Harri Feng <harri.feng@gmail.com>
2014-12-15 00:28:41 -0800

    9th

--- src/main/java/org/hfeng/oj/leet/permutationsequence/Solution.java
+++ src/main/java/org/hfeng/oj/leet/permutationsequence/Solution.java
@@ -1,27 +1,25 @@
 package org.hfeng.oj.leet.permutationsequence;
 public class Solution {
-    private static final String NUM= "123456789";
     public String getPermutation(int n, int k) {
-        if (n == 1) {
-            return "1";
-        }
+        k--;
+        StringBuilder num = new StringBuilder("123456789");
         StringBuilder ret = new StringBuilder();
-        StringBuilder start = new StringBuilder(NUM.substring(0, n));
-        for (int i = n; i >= 1; i--) {
-            // Be careful here, you should use i, other than n
-            // in the loop!!
-            int target = (k - 1) / factorial(i - 1);
-            k -= target * factorial(i - 1);
-            ret.append(start.charAt(target));
-            start.deleteCharAt(target);
+
+        int sum = getAcc(n);
+        for (int i = n; i > 0; i--) {
+            sum /= i;
+            int current = k / sum;
+            ret.append(num.charAt(current));
+            num.deleteCharAt(current);
+            k -= current * sum;
         }
         return ret.toString();
     }
-    private int factorial(int num) {
-        int sum = 1;
-        for (int i = 2; i <= num; i++) {
-            sum *= i;
+    private int getAcc(int n) {
+        int ret = 1;
+        for (int i = 1; i <= n; i++) {
+            ret *= i;
         }
-        return sum;
+        return ret;
     }
 }
