f9211f7805a11c929928544b73f7d0d9ad8bd356
Harri Feng <harri.feng@gmail.com>
2014-12-15 00:08:57 -0800

    8th

--- src/main/java/org/hfeng/oj/leet/palindromenumber/Solution.java
+++ src/main/java/org/hfeng/oj/leet/palindromenumber/Solution.java
@@ -1,30 +1,24 @@
 package org.hfeng.oj.leet.palindromenumber;
-
 public class Solution {
     public boolean isPalindrome(int x){
         if (x < 0) {
             return false;
         }
-
-        if (x < 10) {
-            return true;
-        }
-
         int tmp = x;
-        int times = 1;
+        int size = 1;
         while (tmp >= 10) {
-            times *= 10;
-            tmp /= 10;
+            size *= 10;
+            tmp = tmp / 10;
         }
 
-        // MISTAKE HERE
-        // 009 can be only show 9, while times always has 1 at front
-        while (times >= 10) {
-            if (x / times != x % 10) {
+        while (size >= 10) {
+            int beg = x / size;
+            int end = x % 10;
+            if (beg != end) {
                 return false;
             }
-            x  = x % times / 10;
-            times /= 100;
+            x = x % size / 10;
+            size /= 100;
         }
         return true;
     }
