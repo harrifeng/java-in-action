7030786521a2cfb491cf1e7d82de61ec786a2c62
Harri Feng <harri.feng@gmail.com>
2014-12-16 21:02:39 -0800

    great understanding for trapping rain water

--- src/main/java/org/hfeng/oj/leet/trappingrainwater/Solution.java
+++ src/main/java/org/hfeng/oj/leet/trappingrainwater/Solution.java
@@ -5,25 +5,31 @@ public class Solution {
         if (A.length < 2) {
             return 0;
         }
-        int left = 0;
-        int right = A.length - 1;
-        int ret = 0;
-        while (left < right) {
-            int min = Math.min(A[left], A[right]);
-            if (A[left] == min) {
-                left++;
-                while (left < right && A[left] < min) {
-                    ret += min - A[left];
-                    left++;
-                }
-            } else {
-                right--;
-                while (left < right && A[right] < min) {
-                    ret += min - A[right];
-                    right--;
-                }
+        int max = 0; // highest one
+        for (int i  = 0; i < A.length; i++) {
+            if (A[i] > A[max]) {
+                max = i;
             }
         }
-        return ret;
+        int water = 0;
+        //peek is temp high value
+        for (int i = 0, peak = 0; i < max; i++) {
+            if (A[i] > peak) {
+                peak = A[i];
+            } else {
+                // We know that there Must be some higher one
+                // later, so we dare to add!
+                water += peak - A[i];
+            }
+        }
+        //top is temp high value
+        for (int i = A.length - 1, top = 0; i > max ; i--) {
+            if (A[i] > top) {
+                top = A[i];
+            } else {
+                water += top - A[i];
+            }
+        }
+        return water;
     }
 }
