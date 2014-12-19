df693b5ff3d2b5a8a428e04cba508af20afe83ef
Harri Feng <harri.feng@gmail.com>
2014-12-18 22:38:33 -0800

    18th

--- src/main/java/org/hfeng/oj/leet/threesum/Solution.java
+++ src/main/java/org/hfeng/oj/leet/threesum/Solution.java
@@ -1,17 +1,12 @@
 package org.hfeng.oj.leet.threesum;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
     public List<List<Integer>> threeSum(int[] num) {
-        List<List<Integer>> ret  = new ArrayList<List<Integer>>();
-
         Arrays.sort(num);
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
         for (int i = 0; i < num.length; i++) {
-            if (i > 0 && num[i] == num[i - 1]) {
-                // MISTAKE HERE
+            if (i > 0 && num[i] == num[i-1]) {
                 continue;
             }
             int beg = i + 1;
@@ -19,25 +14,20 @@ public class Solution {
             while (beg < end) {
                 int sum = num[i] + num[beg] + num[end];
                 if (sum == 0) {
-                    List<Integer> li = new ArrayList<Integer>();
-                    li.add(num[i]);
-                    li.add(num[beg]);
-                    li.add(num[end]);
-                    ret.add(li);
-                    // MISTAKE HERE
-                    while (beg < end && num[beg + 1] == num[beg] && num[end - 1] == num[end]) {
+                    List<Integer> one = new ArrayList<Integer>();
+                    one.add(num[i]);
+                    one.add(num[beg]);
+                    one.add(num[end]);
+                    ret.add(one);
+                    while (beg < end && num[beg] == num[beg+1] && num[end] == num[end-1]) {
                         beg++;
                         end--;
                     }
                     beg++;
                     end--;
                 } else if (sum < 0) {
-                    // No need to judge whether the num[beg+1] and num[beg]
-                    // because if num[beg] is NOT EQUAL, num[beg+1] is NOT
-                    // EQUAL either!!
                     beg++;
                 } else {
-                    // Same with sum < 0
                     end--;
                 }
             }
