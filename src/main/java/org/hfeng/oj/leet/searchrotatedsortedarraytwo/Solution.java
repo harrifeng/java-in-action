7d6d742f6a9ad41953d26b0431ce8ee611a01200
Harri Feng <harri.feng@gmail.com>
2014-12-18 21:58:34 -0800

    17 with new understanding

--- src/main/java/org/hfeng/oj/leet/searchrotatedsortedarraytwo/Solution.java
+++ src/main/java/org/hfeng/oj/leet/searchrotatedsortedarraytwo/Solution.java
@@ -2,32 +2,42 @@ package org.hfeng.oj.leet.searchrotatedsortedarraytwo;
 
 public class Solution {
     public boolean search(int A[], int target) {
-        int first = 0;
-        int last = A.length - 1;
-
-        while (first <= last) {
-            int mid = (first + last) / 2;
+        int beg = 0;
+        int end = A.length - 1;
+        while (beg <= end) {
+            int mid = (beg + end) >>> 1;
             if (A[mid] == target) {
                 return true;
             }
-
-            if (A[first] < A[mid]) {
-                if (A[first] <= target && target < A[mid]) {
-                    last = mid;
+            if (A[beg] < A[mid]) {
+                // left is sorted
+                if (A[beg] <= target && target < A[mid]) {
+                    end = mid - 1;
                 } else {
-                    first = mid + 1;
+                    beg = mid + 1;
                 }
-            } else if (A[first] > A[mid]) {
-                if (A[mid] < target && target <= A[last]) {
-                    first = mid + 1;
+            } else if (A[beg] > A[mid]){
+                // right is sorted
+                if (A[mid] < target && target <= A[end]) {
+                    beg = mid + 1;
                 } else {
-                    last = mid;
+                    end = mid - 1;
                 }
             } else {
-                //skip duplicate one
-                first++;
+                // skip duplicate one
+                beg++;
             }
         }
         return false;
     }
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// Difference with searchrotatedsortedarray:                                                         //
+// + if duplicated elements are allowed, The logic 'A[beg] <= A[mid] will show [beg, mid] is sorted' //
+//   is not correct anymore. For example [1, 3, 1, 1, 1, 1] => (beg = 0, mid = 3)                    //
+// + so we split previous two coditions to three ones:                                               //
+//   - A[beg] < A[mid]  ==> left sorted                                                              //
+//   - A[beg] > A[mid]  ==> right sorted                                                             //
+//   - A[beg] == A[mid] ==> skip current beg (beg++)                                                 //
+///////////////////////////////////////////////////////////////////////////////////////////////////////
