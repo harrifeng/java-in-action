93701c99c04e0ae0847ec969563330be623239b7
harri feng <harri.feng@gmail.com>
2014-12-20 18:39:29 +0800

    24, remember to check

--- src/main/java/org/hfeng/oj/leet/zigzagconversion/Solution.java
+++ src/main/java/org/hfeng/oj/leet/zigzagconversion/Solution.java
@@ -1,32 +1,26 @@
 package org.hfeng.oj.leet.zigzagconversion;
-
 public class Solution {
     public String convert(String s, int nRows) {
-        // MISTAKE HERE
         if (nRows < 2) {
             return s;
         }
-        StringBuilder[] sbs = new StringBuilder[nRows];
-
-        for (int i = 0; i < sbs.length; i++) {
-            sbs[i] = new StringBuilder();
+        int group = 2 * nRows - 2;
+        StringBuilder[] sb = new StringBuilder[nRows];
+        for (int i = 0; i < sb.length; i++) {
+            sb[i] = new StringBuilder();
         }
-
-        int group = nRows * 2 - 2;
-
         for (int i = 0; i < s.length(); i++) {
             int pos = i % group;
             if (pos < nRows) {
-                // MISTAKE HERE
-                sbs[pos].append(s.charAt(i));
+                sb[pos].append(s.charAt(i));
             } else {
-                sbs[group - pos].append(s.charAt(i));
+                sb[group - pos].append(s.charAt(i));
             }
         }
 
         StringBuilder ret = new StringBuilder();
-        for (int i = 0; i < sbs.length; i++) {
-            ret.append(sbs[i]);
+        for (int i = 0; i < sb.length; i++) {
+            ret.append(sb[i]);
         }
         return ret.toString();
     }
