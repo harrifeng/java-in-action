e83cb94fdcb63432fdc0e2068712a323886624d2
harri feng <harri.feng@gmail.com>
2014-12-20 17:39:27 +0800

    19th

--- src/main/java/org/hfeng/oj/leet/threesumclosest/Solution.java
+++ src/main/java/org/hfeng/oj/leet/threesumclosest/Solution.java
@@ -1,30 +1,28 @@
 package org.hfeng.oj.leet.threesumclosest;
-
 import java.util.Arrays;
-
 public class Solution {
     public int threeSumClosest(int[] num, int target) {
         if (num.length < 3) {
             return -1;
         }
         Arrays.sort(num);
-
-        // MISTAKE HERE
         int closest = num[0] + num[1] + num[2];
         for (int i = 0; i < num.length; i++) {
-
+            if (i > 0 && num[i] == num[i-1]) {
+                continue;
+            }
             int beg = i + 1;
             int end = num.length - 1;
 
             while (beg < end) {
-                int sum = num[beg] + num[end] + num[i];
+                int sum = num[i] + num[beg] + num[end];
+                if (sum - target == 0) {
+                    return sum;
+                }
                 if (Math.abs(sum - target) < Math.abs(closest - target)) {
                     closest = sum;
                 }
-
-                if (sum == target) {
-                    return sum;
-                } else if (sum < target) {
+                if (sum < target) {
                     beg++;
                 } else {
                     end--;
