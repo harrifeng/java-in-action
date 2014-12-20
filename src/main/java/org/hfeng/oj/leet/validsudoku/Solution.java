64a0855666940989994de2f629b904aab2c77406
harri feng <harri.feng@gmail.com>
2014-12-20 18:31:22 +0800

    23

--- src/main/java/org/hfeng/oj/leet/validsudoku/Solution.java
+++ src/main/java/org/hfeng/oj/leet/validsudoku/Solution.java
@@ -1,12 +1,11 @@
 package org.hfeng.oj.leet.validsudoku;
-
 import java.util.*;
 
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        List<boolean[]> lineList = new ArrayList<boolean[]>();
-        List<boolean[]> columnList = new ArrayList<boolean[]>();
-        List<boolean[]> squareList = new ArrayList<boolean[]>();
+        ArrayList<boolean[]> lineList = new ArrayList<boolean[]>();
+        ArrayList<boolean[]> columnList = new ArrayList<boolean[]>();
+        ArrayList<boolean[]> squareList = new ArrayList<boolean[]>();
 
         for (int i = 0; i < 9; i++) {
             lineList.add(new boolean[9]);
@@ -16,16 +15,18 @@ public class Solution {
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if(board[i][j] == '.') {
+                if (board[i][j] == '.') {
                     continue;
                 }
+
                 int num = board[i][j] - '1';
-                if(lineList.get(i)[num] || columnList.get(j)[num] || squareList.get(i / 3 * 3 + j/ 3)[num]) {
+                if (lineList.get(i)[num] || columnList.get(j)[num] || squareList.get( i/3*3 + j/3)[num]) {
                     return false;
+                } else {
+                    lineList.get(i)[num] = true;
+                    columnList.get(j)[num] = true;
+                    squareList.get( i/3*3 + j/3)[num] = true;
                 }
-                lineList.get(i)[num] = true;
-                columnList.get(j)[num] = true;
-                squareList.get(i / 3 * 3 + j / 3)[num] = true;
             }
         }
         return true;
