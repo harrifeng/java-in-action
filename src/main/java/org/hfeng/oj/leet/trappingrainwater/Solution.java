a363bca80b122d84e854223544415b2b97177839
harri feng <harri.feng@gmail.com>
2014-12-20 17:45:26 +0800

    20th

--- src/main/java/org/hfeng/oj/leet/trappingrainwater/Solution.java
+++ src/main/java/org/hfeng/oj/leet/trappingrainwater/Solution.java
@@ -1,27 +1,28 @@
 package org.hfeng.oj.leet.trappingrainwater;
-
 public class Solution {
     public int trap(int[] A) {
         if (A.length < 2) {
             return 0;
         }
-        int left = 0;
-        int right = A.length - 1;
+        int max = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > A[max]) {
+                max = i;
+            }
+        }
         int ret = 0;
-        while (left < right) {
-            int min = Math.min(A[left], A[right]);
-            if (A[left] == min) {
-                left++;
-                while (left < right && A[left] < min) {
-                    ret += min - A[left];
-                    left++;
-                }
+        for (int i = 0, top = 0; i < max; i++) {
+            if (A[i] > top) {
+                top = A[i];
             } else {
-                right--;
-                while (left < right && A[right] < min) {
-                    ret += min - A[right];
-                    right--;
-                }
+                ret += top - A[i];
+            }
+        }
+        for (int i = A.length - 1, top = 0; i > max; i--) {
+            if (A[i] > top) {
+                top = A[i];
+            } else {
+                ret += top - A[i];
             }
         }
         return ret;
