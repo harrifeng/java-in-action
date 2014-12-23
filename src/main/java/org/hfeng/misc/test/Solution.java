834eefff60cd2f04bcfef52e30a9aec7830727a6
Harri Feng <harri.feng@gmail.com>
2014-12-23 01:49:11 -0800

    new leet

--- src/main/java/org/hfeng/misc/test/Solution.java
+++ src/main/java/org/hfeng/misc/test/Solution.java
@@ -16,9 +16,13 @@ public class Solution {
         }
     }
     public static void main(String[] args) {
-        int[] array = {1, 2, 3};
-        Solution solution = new Solution();
-        ArrayList<List<Integer>> ret = new ArrayList<List<Integer>>();
-        solution.printList(array, 0, new ArrayList<Integer>(), ret);
+        int[][] matrix = new int[][]{ {1, 2}, {3, 4}, {5, 6}};
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+
     }
 }
