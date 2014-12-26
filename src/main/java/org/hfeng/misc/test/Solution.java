559079825e2a3c4808649449ef1bc733f23522fa
Harri Feng <harri.feng@gmail.com>
2014-12-26 01:23:08 -0800

    add candy

--- src/main/java/org/hfeng/misc/test/Solution.java
+++ src/main/java/org/hfeng/misc/test/Solution.java
@@ -17,11 +17,9 @@ public class Solution {
     }
     public static void main(String[] args) {
         int[][] matrix = new int[][]{ {1, 2}, {3, 4}, {5, 6}};
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]);
-            }
-            System.out.println();
+
+        for (int i = 0; i < -3; i++) {
+            System.out.println("hello");
         }
 
     }
---
+++ src/main/java/org/hfeng/oj/leet/candy/Solution.java
@@ -0,0 +1,32 @@
+package org.hfeng.oj.leet.candy;
+
+public class Solution {
+    public int candy(int[] ratings) {
+        if (ratings.length == 1) {
+            return 1;
+        }
+
+        int[] value = new int[ratings.length];
+        for (int i = 1, inc = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                value[i] = Math.max(inc++, value[i]);
+            } else {
+                inc = 1;
+            }
+        }
+
+        for (int i = ratings.length - 2, inc = 1; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                value[i] = Math.max(inc++, value[i]);
+            } else {
+                inc = 1;
+            }
+        }
+
+        int ret = ratings.length;
+        for (int i = 0; i < value.length; i++) {
+            ret += value[i];
+        }
+        return ret;
+    }
+}
