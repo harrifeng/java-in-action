bceebe12d6112ae5cf686898810dd0bfc3e8b192
harri feng <harrifeng@gmail.com>
2015-01-08 21:03:54 +0800

    29

--- src/main/java/org/hfeng/oj/leet/trappingrainwater/Solution.java
+++ src/main/java/org/hfeng/oj/leet/trappingrainwater/Solution.java
@@ -1,35 +1,32 @@
 package org.hfeng.oj.leet.trappingrainwater;
-
 public class Solution {
     public int trap(int[] A) {
         if (A.length < 2) {
             return 0;
         }
-        int max = 0; // highest one
-        for (int i  = 0; i < A.length; i++) {
-            if (A[i] > A[max]) {
+        int max = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[max] < A[i]) {
                 max = i;
             }
         }
-        int water = 0;
-        //peek is temp high value
-        for (int i = 0, peak = 0; i < max; i++) {
-            if (A[i] > peak) {
-                peak = A[i];
-            } else {
-                // We know that there Must be some higher one
-                // later, so we dare to add!
-                water += peak - A[i];
-            }
-        }
-        //top is temp high value
-        for (int i = A.length - 1, top = 0; i > max ; i--) {
-            if (A[i] > top) {
+
+        int ret = 0;
+        for (int i = 0, top = 0; i < max; i++) {
+            if (top < A[i]) {
                 top = A[i];
             } else {
-                water += top - A[i];
+                ret += (top - A[i]);
             }
         }
-        return water;
+
+        for (int i = A.length - 1, summit = 0; i > max; i--) {
+            if (summit < A[i]) {
+                summit = A[i];
+            } else {
+                ret += (summit - A[i]);
+            }
+        }
+        return ret;
     }
 }
