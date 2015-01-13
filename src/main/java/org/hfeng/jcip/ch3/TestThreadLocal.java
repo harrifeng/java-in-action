80a3a18636b6772c0019eb4eed38c592572db6cd
harri feng <harrifeng@gmail.com>
2015-01-13 11:23:50 +0800

    before 3.5

---
+++ src/main/java/org/hfeng/jcip/ch3/TestThreadLocal.java
@@ -0,0 +1,54 @@
+package org.hfeng.jcip.ch3;
+
+class Connection {
+    private String url;
+
+    Connection(String url) {
+        this.url = url;
+    }
+}
+
+class DriverManager {
+
+    public static Connection getConnection(String db_url) {
+        return new Connection(db_url);
+    }
+}
+
+public class TestThreadLocal {
+    public static final String DB_URL = "jdbc:mysql://localhost/mydatabase";
+
+    private ThreadLocal<Connection> connectionHolder = new ThreadLocal<Connection>() {
+        public Connection initialValue() {
+            return DriverManager.getConnection(DB_URL);
+        }
+    };
+
+    private class Thread1 extends Thread {
+        public void run() {
+            System.out.println("Connection is " + connectionHolder.get());
+        }
+    }
+
+    private class Thread2 extends Thread {
+        public void run() {
+            System.out.println("Connection is " + connectionHolder.get());
+        }
+    }
+
+    public void display() {
+        new Thread1().start();
+        new Thread2().start();
+    }
+
+    public static void main(String[] args) {
+        TestThreadLocal testThreadLocal = new TestThreadLocal();
+        testThreadLocal.display();
+    }
+}
+
+////////////////////////////////////////////////////////
+// <===================OUTPUT===================>     //
+// Connection is org.hfeng.jcip.ch3.Connection@fe268a //
+// Connection is org.hfeng.jcip.ch3.Connection@52b16b //
+////////////////////////////////////////////////////////
