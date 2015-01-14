6642490aacc306f160dbe5ece289090f1b0ebc3e
Haoran Feng <haoran.feng@sap.com>
2015-01-13 22:20:25 -0800

    import to add package

--- util/newleet.py
+++ util/newleet.py
@@ -13,22 +13,24 @@ class Solution(object):
         test_fd = os.path.join(root_fd, ('src/test/java/org/hfeng/oj/leet' +
                                            '/%s') % pkg_name)
         test_file = os.path.join(test_fd, "SolutionTest.java")
+        pkg_loc = "package org.hfeng.oj.leet.%s;" % pkg_name
         self.src_fd = src_fd
         self.test_fd = test_fd
         self.src_file = src_file
         self.test_file = test_file
+        self.pkg_loc = pkg_loc
 
     def new_leet(self):
         print "---------->handling------" +  self.src_fd
         if not os.path.exists(self.src_fd):
             os.mkdir(self.src_fd)
         print "---------->handling------" +  self.src_file
-        open(self.src_file, 'a').close()
+        with open(self.src_file, 'a') as f: f.write(self.pkg_loc)
         print "---------->handling------" +  self.test_fd
         if not os.path.exists(self.test_fd):
             os.mkdir(self.test_fd)
         print "---------->handling------" +  self.test_file
-        open(self.test_file, 'a').close()
+        with open(self.test_file, 'a') as f: f.write(self.pkg_loc)
 
 def main():
     parser = optparse.OptionParser(__doc__.strip())
