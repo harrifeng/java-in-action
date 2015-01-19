ae3a2d47904614081cbe4e3cc7c902f702ca7178
Haoran Feng <haoran.feng@sap.com>
2015-01-18 18:58:49 -0800

    Before 5.2

---
+++ src/main/java/org/hfeng/book/jcip/ch5/UnSafeVectorHelpers.java
@@ -0,0 +1,15 @@
+package org.hfeng.book.jcip.ch5;
+
+import java.util.Vector;
+
+public class UnSafeVectorHelpers {
+    public static Object getLast(Vector list) {
+        int lastIndex = list.size() - 1;
+        return list.get(lastIndex);
+    }
+
+    public static void deleteLast(Vector list) {
+        int lastIndex = list.size() - 1;
+        list.remove(lastIndex);
+    }
+}
