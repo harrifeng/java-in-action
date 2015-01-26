ee6803cc3d917e84a8201faa1631c2180167e68e
Haoran Feng <haoran.feng@sap.com>
2015-01-26 15:58:14 +0800

    Helpful explanation from https://www.evernote.com/shard/s444/sh/6b4c1cb0-81da-4478-94fa-d19b6fc240ff/b73bd0e2b528174e6f00d8c35acae3c5

---
+++ src/main/java/org/hfeng/oj/leet/linkedlistcycletwo/Solution.java
@@ -0,0 +1,27 @@
+package org.hfeng.oj.leet.linkedlistcycletwo;
+
+import org.hfeng.oj.leet.util.ListNode;
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                ListNode slow2 = head;
+
+                while (slow2 != slow) {
+                    slow2 = slow2.next;
+                    slow = slow.next;
+                }
+                return slow2;
+            }
+        }
+
+        return null;
+    }
+}
