c63f3f22db4c3e10c92cc8e8ca0ac5f07c06fa5f
harri feng <harrifeng@gmail.com>
2015-01-28 21:04:47 +0800

    some chapter 6

---
+++ src/main/java/org/hfeng/misc/jdp/ch6/ReadWriteLock.java
@@ -0,0 +1,39 @@
+package org.hfeng.misc.jdp.ch6;
+
+public final class ReadWriteLock {
+    private int readlingReaders = 0;
+    private int waitingWriters = 0;
+    private int writingWriters = 0;
+    private boolean preferWriter = true;
+
+    public synchronized void readLock() throws InterruptedException {
+        while (writingWriters > 0 || (preferWriter && waitingWriters >0)) {
+            wait();
+        }
+        readlingReaders++;
+    }
+
+    public synchronized void readUnlock() {
+        readlingReaders--;
+        preferWriter = true;
+        notifyAll();
+    }
+
+    public synchronized void writeLock() throws InterruptedException {
+        waitingWriters++;
+        try {
+            while (readlingReaders > 0 || writingWriters > 0) {
+                wait();
+            }
+        } finally {
+            waitingWriters--;
+        }
+        writingWriters++;
+    }
+
+    public synchronized void writeUnLock() {
+        writingWriters--;
+        preferWriter = false;
+        notifyAll();
+    }
+}
