147075c3cb0817a6deda2c24e3483e38963881bc
harri feng <harrifeng@gmail.com>
2015-01-31 21:07:33 +0800

    new leet

---
+++ src/test/java/org/hfeng/oj/leet/validpalindrome/SolutionTest.java
@@ -0,0 +1,31 @@
+package org.hfeng.oj.leet.validpalindrome;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SolutionTest {
+    Solution solution;
+
+    @Before
+    public void before() throws Exception {
+        solution = new Solution();
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    @Test
+    public void testIsValidPalindrome() throws Exception {
+        assertTrue(solution.isPalindrome(""));
+        assertTrue(solution.isPalindrome("abcdcba"));
+        assertTrue(solution.isPalindrome("A man, a plan, a canal: Panama"));
+        assertFalse(solution.isPalindrome("race a car"));
+    }
+
+
+}
