366b08c9b43a3f6f05988d6182a1fde164462a8b
Haoran Feng <haoran.feng@sap.com>
2015-02-03 21:37:18 -0800

    great solution to pass all tests

--- src/main/java/org/hfeng/oj/leet/wildcardmatching/Solution.java
+++ src/main/java/org/hfeng/oj/leet/wildcardmatching/Solution.java
@@ -2,31 +2,31 @@ package org.hfeng.oj.leet.wildcardmatching;
 
 public class Solution {
     public boolean isMatch(String s, String p) {
-        if (p.length() == 0) {
-            return s.length() == 0;
-        }
-        // For leetcode one special test, will ignore this case, as the complexity is enough
-        if (s.length() > 300 && p.charAt(0) == '*' && p.charAt(p.length() - 1) == '*') {
+        String star = new String();
+        String ss = s;
+        while (s.length() > 0) {
+            if (p.length() > 0 && (p.charAt(0) == '?' || p.charAt(0) == s.charAt(0))) {
+                s = s.substring(1);
+                p = p.substring(1);
+                continue;
+            }
+            if (p.length() > 0 && p.charAt(0) == '*') {
+                star = p;
+                p = p.substring(1);
+                ss = s;
+                continue;
+            }
+            if (star.length() > 0) {
+                p = star.substring(1);
+                ss = ss.substring(1);
+                s = ss;
+                continue;
+            }
             return false;
         }
-        boolean[] res = new boolean[s.length() + 1];
-        res[0] = true;
-        for (int j = 0; j < p.length(); j++) {
-            if (p.charAt(j) != '*') {
-                for (int i = s.length() - 1; i >= 0; i--) {
-                    res[i + 1] = res[i] && (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j));
-                }
-            } else {
-                int i = 0;
-                while (i <= s.length() && !res[i]) {
-                    i++;
-                }
-                for (; i <= s.length(); i++) {
-                    res[i] = true;
-                }
-            }
-            res[0] = res[0] && p.charAt(j) == '*';
+        while (p.length() > 0 && p.charAt(0) == '*') {
+            p = p.substring(1);
         }
-        return res[s.length()];
+        return p.length() == 0;
     }
 }
