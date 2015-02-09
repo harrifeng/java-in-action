f74c6de220f6ecf52b5efa2118e7d619eafb9ee5
Haoran Feng <haoran.feng@sap.com>
2015-02-09 02:17:59 -0800

    finish chapter 6

--- src/main/java/org/hfeng/misc/test/HelloWorld.java
+++ src/main/java/org/hfeng/misc/test/HelloWorld.java
@@ -3,10 +3,13 @@ package org.hfeng.misc.test;
 
 import org.hfeng.oj.leet.util.ArrayHelper;
 
+import java.util.concurrent.FutureTask;
+
 public class HelloWorld {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(ArrayHelper.randInt(0,9));
         }
+
     }
 }
