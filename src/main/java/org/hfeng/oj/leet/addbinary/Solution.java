28ece54a9ef939d8bb994ab8993a951edd0cc5f7
Haoran Feng <haoran.feng@sap.com>
2015-02-26 19:06:17 -0800

    0th

--- src/main/java/org/hfeng/oj/leet/addbinary/Solution.java
+++ src/main/java/org/hfeng/oj/leet/addbinary/Solution.java
@@ -1,20 +1,18 @@
 package org.hfeng.oj.leet.addbinary;
-
 public class Solution {
     public String addBinary(String a, String b) {
-        int carry = 0;
-        StringBuilder result = new StringBuilder();
-
-        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
-            int aj = (i >= 0 ? Character.digit(a.charAt(i), 10) : 0);
-            int bj = (j >= 0 ? Character.digit(b.charAt(j), 10) : 0);
-            int val = (aj + bj + carry) % 2;
-            carry = (aj + bj + carry) / 2;
-            result.insert(0, Integer.toString(val));
+        StringBuilder sb = new StringBuilder();
+        int adv = 0;
+        for (int i = 0; i < a.length() || i < b.length(); i++) {
+            char cha = a.length() > i ? a.charAt(a.length() - i - 1) : '0';
+            char chb = b.length() > i ? b.charAt(b.length() - i - 1) : '0';
+            int sum = adv + (cha - '0') + (chb - '0');
+            sb.insert(0, (sum % 2)) ;
+            adv = sum / 2;
         }
-        if (carry == 1) {
-            result.insert(0, Integer.toString(1));
+        if (adv != 0) {
+            sb.insert(0, '1');
         }
-        return result.toString();
+        return sb.toString();
     }
 }
