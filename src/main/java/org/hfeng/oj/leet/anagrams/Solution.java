9fb04b65edfc6c9db3e3778e45f5a0ad58d2f408
Haoran Feng <haoran.feng@sap.com>
2015-02-26 21:57:25 -0800

    2nd

--- src/main/java/org/hfeng/oj/leet/anagrams/Solution.java
+++ src/main/java/org/hfeng/oj/leet/anagrams/Solution.java
@@ -5,37 +5,29 @@ import java.util.*;
 public class Solution {
     public List<String> anagrams(String[] strs) {
         List<String> ret = new ArrayList<String>();
+
         Map<String, String> map = new HashMap<String, String>();
 
         for (int i = 0; i < strs.length; i++) {
-            if (map.containsKey(formalOrder(strs[i]))) {
+            if (map.containsKey(sortChars(strs[i]))) {
                 ret.add(strs[i]);
             } else {
-                map.put(formalOrder(strs[i]), strs[i]);
+                map.put(sortChars(strs[i]), strs[i]);
             }
         }
 
         for (int i = 0; i < ret.size(); i++) {
-            if (map.containsKey(formalOrder(ret.get(i)))) {
-                ret.add(map.get(formalOrder(ret.get(i))));
-                map.remove(formalOrder(ret.get(i)));
+            if (map.containsKey(sortChars(ret.get(i)))) {
+                ret.add(map.get(sortChars(ret.get(i))));
+                map.remove(sortChars(ret.get(i)));
             }
         }
         return ret;
     }
 
-    private String formalOrder(String str) {
-
-        int[] ret = new int[str.length()];
-        for (int i = 0; i < str.length(); i++) {
-            ret[i] = str.charAt(i) - 'a';
-        }
-        Arrays.sort(ret);
-        StringBuilder pivot = new StringBuilder();
-        for (int i = 0; i < ret.length; i++) {
-            pivot.append(Integer.toString(ret[i] + 'a'));
-        }
-        return pivot.toString();
+    private String sortChars(String str) {
+        char[] content = str.toCharArray();
+        Arrays.sort(content);
+        return new String(content);
     }
-
 }
