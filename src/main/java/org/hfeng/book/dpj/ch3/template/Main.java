03bbdd5e6d59356edecea7ef447ca42c56e7e20c
harri feng <harrifeng@gmail.com>
2015-02-27 22:20:06 +0800

    template

---
+++ src/main/java/org/hfeng/book/dpj/ch3/template/Main.java
@@ -0,0 +1,31 @@
+package org.hfeng.book.dpj.ch3.template;
+
+public class Main {
+    public static void main(String[] args) {
+        AbstractDisplay d1 = new CharDisplay('H');
+        AbstractDisplay d2 = new StringDisplay("Hello World");
+        AbstractDisplay d3 = new StringDisplay("Welcome Back");
+        d1.display();
+        d2.display();
+        d3.display();
+    }
+}
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// <<HHHHH>>                                      //
+// +-----------+                                  //
+// |Hello World|                                  //
+// |Hello World|                                  //
+// |Hello World|                                  //
+// |Hello World|                                  //
+// |Hello World|                                  //
+// +-----------+                                  //
+// +------------+                                 //
+// |Welcome Back|                                 //
+// |Welcome Back|                                 //
+// |Welcome Back|                                 //
+// |Welcome Back|                                 //
+// |Welcome Back|                                 //
+// +------------+                                 //
+////////////////////////////////////////////////////
