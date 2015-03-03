b91bb314da4f7a8094a878e952ed87bd24c7072c
Haoran Feng <haoran.feng@sap.com>
2015-03-02 23:19:28 -0800

    3rd

--- src/main/java/org/hfeng/oj/leet/atoi/Solution.java
+++ src/main/java/org/hfeng/oj/leet/atoi/Solution.java
@@ -1,39 +1,37 @@
 package org.hfeng.oj.leet.atoi;
-
 public class Solution {
     public int atoi(String str) {
-        if (str.length() == 0) {
+        if (str.length() == 0 || str == null) {
             return 0;
         }
         int start = 0;
         while (str.charAt(start) == ' ') {
             start++;
         }
-
-        boolean negative = false;
-
+        boolean neg = false;
         if (str.charAt(start) == '-') {
-            negative = true;
+            neg = true;
             start++;
         } else if (str.charAt(start) == '+') {
+            neg = false;
             start++;
         }
-
         int ret = 0;
+
         for (int i = start; i < str.length(); i++) {
-            // USEFUL HERE
             if (Character.isDigit(str.charAt(i))) {
+                int cur = Character.digit(str.charAt(i), 10);
                 if (ret > Integer.MAX_VALUE / 10
-                        || (ret == Integer.MAX_VALUE / 10
-                        && Character.digit(str.charAt(i), 10) > Integer.MAX_VALUE % 10)) {
-                    return negative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                    || (ret == Integer.MAX_VALUE / 10
+                        && cur > Integer.MAX_VALUE % 10)) {
+                    return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                } else {
+                    ret = ret * 10 + cur;
                 }
-                // USEFUL HERE
-                ret = ret * 10 + Character.digit(str.charAt(i), 10);
             } else {
                 break;
             }
         }
-        return negative ? (-1) * ret : ret;
+        return neg ? (-1) * ret : ret;
     }
 }
