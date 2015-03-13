bfc64367d24138bb19b04f00115e7c7adb1adc4a
Haoran Feng <haoran.feng@sap.com>
2015-03-12 19:41:26 -0700

    observable in java diff

--- src/main/java/org/hfeng/misc/hfdp/ch2/observer/observable/WeatherStation.java
+++ src/main/java/org/hfeng/misc/hfdp/ch2/observer/observable/WeatherStation.java
@@ -5,11 +5,22 @@ public class WeatherStation {
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay currentConditionDisplay =
             new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+
+        weatherData.setMeasurements(82, 70, 29.2f);
+        System.out.println();
         weatherData.setMeasurements(82, 70, 29.2f);
     }
 }
 
 //////////////////////////////////////////////////////////
 // <===================OUTPUT===================>       //
+// Forecast: Watch out for cooler, rainy weather        //
+// Avg/Max/Min temperate = 82.0/82.0/82.0               //
+// Current conditions: 82.0F degrees and 82.0% humidity //
+//                                                      //
+// Forecast: More of the same                           //
+// Avg/Max/Min temperate = 82.0/82.0/82.0               //
 // Current conditions: 82.0F degrees and 82.0% humidity //
 //////////////////////////////////////////////////////////
