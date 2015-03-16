54ed20aa6081215eef94b042ef0170911621c22d
Haoran Feng <haoran.feng@sap.com>
2015-03-15 18:13:51 -0700

    9th from 6p

--- src/main/java/org/hfeng/oj/leet/gasstation/Solution.java
+++ src/main/java/org/hfeng/oj/leet/gasstation/Solution.java
@@ -2,31 +2,19 @@ package org.hfeng.oj.leet.gasstation;
 
 public class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int len = gas.length;
-        int[] diff = new int[len];
-
-        for (int i = 0; i < len; i++) {
-            diff[i] = gas[i] - cost[i];
-        }
-
         int leftGas = 0;
         int sum = 0;
-        int startNode = 0;
-
-        for (int i = 0; i < len; i++) {
-            leftGas += diff[i];
-            sum += diff[i];
-            // They must be bigger than Zero at the very first round
+        int startPoint = 0;
+        for (int i = 0; i < gas.length; i++) {
+            leftGas += gas[i] - cost[i];
+            sum += gas[i] - cost[i];
+            // sum should be bigger than Zero at the very first round.
             if (sum < 0) {
-                startNode = i + 1;
+                // Failed here, hope startPoint can be next one.
+                startPoint = i + 1;
                 sum = 0;
             }
         }
-
-        if (leftGas < 0) {
-            return -1;
-        } else {
-            return startNode;
-        }
+        return leftGas >= 0 ? startPoint : -1;
     }
 }
