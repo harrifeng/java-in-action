54856dcd5f2f02be35a55d8f5ba7f18ad23ab634
harri feng <harrifeng@gmail.com>
2015-03-19 21:42:59 +0800

    KMP is hard to understand

---
+++ src/main/java/org/hfeng/misc/algo/KMP.java
@@ -0,0 +1,70 @@
+package org.hfeng.misc.algo;
+
+public class KMP {
+
+    private int[] next;
+
+    public int violentMatch(String str, String pattern) {
+        int i = 0;
+        int j = 0;
+        while (i < str.length() && j < pattern.length()) {
+            if (str.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                i = i - j + 1;
+                j = 0;
+            }
+        }
+        if (j == pattern.length()) {
+            return i - j;
+        } else {
+            return -1;
+        }
+    }
+
+    public int kmpMatch(String str, String pattern) {
+        next = getNext(pattern);
+        int i = 0;
+        int j = 0;
+        while (i < str.length() && j < pattern.length()) {
+            if (j == -1 || str.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+        }
+        if (j == pattern.length()) {
+            return i - j;
+        } else {
+            return -1;
+        }
+    }
+
+    private int[] getNext(String p) {
+        int[] next = new int[p.length()];
+        next[0] = -1;
+        int k = -1;
+        int j = 0;
+        while (j < p.length() - 1) {
+            if (k == -1 || p.charAt(j) == p.charAt(k)) {
+                ++k;
+                ++j;
+                next[j] = k;
+            } else {
+                k = next[k];
+            }
+        }
+        return next;
+    }
+
+    public static void main(String[] args) {
+        KMP kmp = new KMP();
+        String str = "bbc abcdab abcdabcdabde";
+        String pattern = "abcdabd";
+
+        System.out.println(kmp.violentMatch(str, pattern));
+        System.out.println(kmp.kmpMatch(str, pattern));
+    }
+}
