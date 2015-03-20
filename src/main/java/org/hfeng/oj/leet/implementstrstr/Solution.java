d366b3a7450d05fadc79b6dc4c43c5d2f00c974f
Haoran Feng <haoran.feng@sap.com>
2015-03-20 00:19:51 -0700

    kmp implementation

--- src/main/java/org/hfeng/oj/leet/implementstrstr/Solution.java
+++ src/main/java/org/hfeng/oj/leet/implementstrstr/Solution.java
@@ -3,13 +3,14 @@ public class Solution {
     public int strStr(String haystack, String needle) {
         int i = 0;
         int j = 0;
+
         while (i < haystack.length() && j < needle.length()) {
-            if (haystack.charAt(i) == needle.charAt(j)) {
+            int[] n = getNext(needle);
+            if (j == -1 || haystack.charAt(i) == needle.charAt(j)) {
                 i++;
                 j++;
             } else {
-                i = i - j + 1;
-                j = 0;
+                j = n[j];
             }
         }
         if (j == needle.length()) {
@@ -17,5 +18,23 @@ public class Solution {
         } else {
             return -1;
         }
+    }
+
+    private int[] getNext(String pattern) {
+        int[] next = new int[pattern.length()];
+        next[0] = -1;
+        int k = -1;
+        int j = 0;
+
+        while (j < pattern.length() - 1) {
+            if (k == -1 || pattern.charAt(j) == pattern.charAt(k)) {
+                ++k;
+                ++j;
+                next[j] = k;
+            } else {
+                k = next[k];
+            }
+        }
+        return next;
     }
 }
