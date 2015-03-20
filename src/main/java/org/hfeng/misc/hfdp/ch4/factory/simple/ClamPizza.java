790257c06fa1807ed61bff70eb39e99dd50e6f24
Haoran Feng <haoran.feng@sap.com>
2015-03-19 17:54:49 -0700

    simple factory

---
+++ src/main/java/org/hfeng/misc/hfdp/ch4/factory/simple/ClamPizza.java
@@ -0,0 +1,11 @@
+package org.hfeng.misc.hfdp.ch4.factory.simple;
+
+public class ClamPizza extends Pizza{
+    public ClamPizza() {
+        name = "Clam Pizza";
+        dough = "Thin crust";
+        sauce = "White garlic sauce";
+        toppings.add("Clams");
+        toppings.add("Crated parmesan cheese");
+    }
+}
