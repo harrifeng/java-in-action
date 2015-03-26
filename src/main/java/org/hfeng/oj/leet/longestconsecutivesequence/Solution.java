7be5e371670e53b9c8af55d64a6ed8a15a566b42
Haoran Feng <haoran.feng@sap.com>
2015-03-26 05:21:43 -0700

    more concise

--- src/main/java/org/hfeng/oj/leet/longestconsecutivesequence/Solution.java
+++ src/main/java/org/hfeng/oj/leet/longestconsecutivesequence/Solution.java
@@ -11,30 +11,27 @@ public class Solution {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         int maxV = 0;
         for (int i = 0; i < num.length; i++) {
-            int sum = 0;
+            int sum = 1;
             if (map.containsKey(num[i])) {
                 continue;
             }
 
             if (map.containsKey(num[i] - 1)) {
-                int now = map.get(num[i] - 1);
-                sum += now;
+                sum += map.get(num[i] - 1);
             }
             if (map.containsKey(num[i] + 1)) {
-                int now = map.get(num[i] + 1);
-                sum += now;
+                sum += map.get(num[i] + 1);
             }
 
             if (map.containsKey(num[i] - 1)) {
-                int now = map.get(num[i] - 1);
-                map.put(num[i] - now, 1 + sum);
+                map.put(num[i] - map.get(num[i] - 1), sum);
             }
             if (map.containsKey(num[i] + 1)) {
-                int now = map.get(num[i] + 1);
-                map.put(num[i] + now, 1 + sum);
+                map.put(num[i] + map.get(num[i] + 1), sum);
             }
-            map.put(num[i], sum + 1);
-            maxV = Math.max(maxV, sum + 1);
+
+            map.put(num[i], sum);
+            maxV = Math.max(maxV, sum);
         }
         return maxV;
     }
