6b338f8b4e4ff525c486c99f58ea0dda2349c0af
Haoran Feng <haoran.feng@sap.com>
2015-03-27 01:53:38 -0700

    17 longestsubstringwithoutrepeat : Be careful to reinitialize all

--- src/main/java/org/hfeng/oj/leet/longestsubstringwithoutrepeat/Solution.java
+++ src/main/java/org/hfeng/oj/leet/longestsubstringwithoutrepeat/Solution.java
@@ -1,22 +1,23 @@
 package org.hfeng.oj.leet.longestsubstringwithoutrepeat;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         int[] index = new int[256];
         Arrays.fill(index, -1);
-        int maxLen = 0;
+
         int len = 0;
+        int maxLen = 0;
         for (int i = 0; i < s.length(); i++) {
             if (index[s.charAt(i)] != -1) {
                 maxLen = Math.max(maxLen, len);
-                len = 0;
                 i = index[s.charAt(i)] + 1;
                 Arrays.fill(index, -1);
+                len = 0;
             }
-            len++;
             index[s.charAt(i)] = i;
+            len++;
         }
         return Math.max(maxLen, len);
     }
