f5a633f2726a7501cd8271a83b2af295e718a1ed
harri feng <harrifeng@gmail.com>
2015-03-28 11:33:40 +0800

    too many breaks

--- src/main/java/org/hfeng/oj/leet/nextpermutation/Solution.java
+++ src/main/java/org/hfeng/oj/leet/nextpermutation/Solution.java
@@ -2,50 +2,41 @@ package org.hfeng.oj.leet.nextpermutation;
 
 public class Solution {
     public void nextPermutation(int[] num) {
-        if (num.length <= 1) {
+        if (num.length == 0 || num.length == 1) {
             return;
         }
-
-        int i, j;
-        // From back to front, find the first
-        // one to break ascending
+        int i = 0;
         for (i = num.length - 2; i >= 0; i--) {
             if (num[i] < num[i + 1]) {
                 break;
             }
         }
-        // if all ascending, it is the biggest
-        // number. Reverse and return
         if (i == -1) {
-            reverseArray(num, 0);
+            reverse(num, 0, num.length - 1);
             return;
         }
 
-        // From back to front, find the first one
-        // that is bigger than i
-        for (j = num.length - 1; j > i; j--) {
-            if (num[j] > num[i]) {
+        int breaker = i + 1;
+        for (int j = num.length - 1; j >= breaker; j--) {
+            if (num[j] > num[breaker - 1]) {
+                swap(num, j, breaker - 1);
                 break;
             }
         }
-
-        swapArrayElement(num, i, j);
-        //reverse the one bigger than i
-        reverseArray(num, i + 1);
+        reverse(num, breaker, num.length - 1);
     }
 
-    private void reverseArray(int[] num, int beg) {
-        int end = num.length - 1;
+    private void swap(int[] num, int left, int right) {
+        int tmp = num[left];
+        num[left] = num[right];
+        num[right] = tmp;
+    }
+
+    private void reverse(int[] num, int beg, int end) {
         while (beg < end) {
-            swapArrayElement(num, beg, end);
+            swap(num, beg, end);
             beg++;
             end--;
         }
-    }
-
-    private void swapArrayElement(int[] num, int a, int b) {
-        int tmp = num[a];
-        num[a] = num[b];
-        num[b] = tmp;
     }
 }
