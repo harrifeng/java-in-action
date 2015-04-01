5b8d838fd292aafe5d0b36dd97b4525e8153933d
Haoran Feng <haoran.feng@sap.com>
2015-04-01 02:10:06 -0700

    regular expression : the key to handle *, you should first determin pattern.substring(2) first

--- src/main/java/org/hfeng/oj/leet/regularexpressionmatching/Solution.java
+++ src/main/java/org/hfeng/oj/leet/regularexpressionmatching/Solution.java
@@ -5,21 +5,21 @@ public class Solution {
             return s.length() == 0;
         }
         if (p.length() == 1) {
-            return (s.length() == 1
-                    && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)));
+            return s.length() == 1
+                && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)) ;
         }
-        // p is bigger than 1 here
-        if (p.charAt(1) != '*') {
-            return (s.length() >= 1
-                    && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
-                    && isMatch(s.substring(1), p.substring(1)));
-        } else {
+        // p.length() i bigger than 1 here
+        if (p.charAt(1) == '*') {
             if (isMatch(s, p.substring(2))) {
                 return true;
             }
-            return (s.length() >= 1
-                    && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))
-                    && isMatch(s.substring(1), p));
+            return s.length() > 0
+                && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))
+                && isMatch(s.substring(1), p);
+        } else {
+            return s.length() > 0
+                && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))
+                && isMatch(s.substring(1), p.substring(1));
         }
     }
 }
