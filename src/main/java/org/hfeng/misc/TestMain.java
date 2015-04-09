ad232b67e5707b2d48396628c5bb2653318dc494
Haoran Feng <haoran.feng@sap.com>
2015-04-09 05:38:51 -0700

    static method can be called by instance

---
+++ src/main/java/org/hfeng/misc/TestMain.java
@@ -0,0 +1,15 @@
+package org.hfeng.misc;
+
+class Test {
+    public static void display() {
+        System.out.println("hello");
+    }
+}
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.display();
+    }
+}
