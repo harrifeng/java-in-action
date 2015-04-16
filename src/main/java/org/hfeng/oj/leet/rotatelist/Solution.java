e04c8ba15cee039dc82b36c957a1a95919266347
Haoran Feng <haoran.feng@sap.com>
2015-04-15 18:41:34 -0700

    my solution, extra variable is not necessary

--- src/main/java/org/hfeng/oj/leet/rotatelist/Solution.java
+++ src/main/java/org/hfeng/oj/leet/rotatelist/Solution.java
@@ -1,23 +1,32 @@
 package org.hfeng.oj.leet.rotatelist;
-import org.hfeng.oj.leet.util.*;
 
+import org.hfeng.oj.leet.util.*;
 public class Solution {
     public ListNode rotateRight(ListNode head, int n) {
-        if (head == null || head.next == null) {
+        if (head == null || head.next == null || n <= 0) {
             return head;
         }
-        int len = 1;
-        ListNode tmp = head;
-        while (tmp.next != null) {
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode last = dummy;
+        int len = 0;
+
+        while (last.next != null) {
             len++;
-            tmp = tmp.next;
+            last = last.next;
         }
-        n %= len;
-        int k = len - n;
-        tmp.next = head;
+
+        last.next = head;
+
+        int k = len - (n % len);
+
+        ListNode tmp = dummy;
         for (int i = 0; i < k; i++) {
             tmp = tmp.next;
         }
+
         ListNode ret = tmp.next;
         tmp.next = null;
         return ret;
