5ff7fb2b4f252f71362a7ff9212f72de6b45c747
Haoran Feng <haoran.feng@sap.com>
2015-04-20 01:27:44 -0700

    clean commit

--- src/main/java/org/hfeng/oj/leet/setmatrixzeroes/Solution.java
+++ src/main/java/org/hfeng/oj/leet/setmatrixzeroes/Solution.java
@@ -30,25 +30,9 @@ public class Solution {
             }
         }
 
-        // for (int i = 1; i < m; i ++) {
-        //     for (int j = 1; j < n; j++) {
-        //         if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-        //             matrix[i][j] = 0;
-        //         }
-        //     }
-        // }
-
-        for (int i = 1; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 1; j < n; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        for (int j = 1; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                for (int i = 1; i < m; i++) {
+        for (int i = 1; i < m; i ++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
