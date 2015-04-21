b0476fc3beb8a9c4c397edaef8cb2616ecaad563
Haoran Feng <haoran.feng@sap.com>
2015-04-21 02:23:36 -0700

    no need the continue

--- src/main/java/org/hfeng/oj/leet/threesumclosest/Solution.java
+++ src/main/java/org/hfeng/oj/leet/threesumclosest/Solution.java
@@ -1,36 +1,36 @@
 package org.hfeng.oj.leet.threesumclosest;
-
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
     public int threeSumClosest(int[] num, int target) {
+        Arrays.sort(num);
         if (num.length < 3) {
             return -1;
         }
-        Arrays.sort(num);
 
-        // MISTAKE HERE
-        int closest = num[0] + num[1] + num[2];
+        int minSum = num[0] + num[1] + num[2];
         for (int i = 0; i < num.length; i++) {
-
+            // if (i > 0 && num[i - 1] == num[i]) {
+            //     continue;
+            // }
             int beg = i + 1;
             int end = num.length - 1;
-
             while (beg < end) {
-                int sum = num[beg] + num[end] + num[i];
-                if (Math.abs(sum - target) < Math.abs(closest - target)) {
-                    closest = sum;
-                }
-
+                int sum = num[i] + num[beg] + num[end];
                 if (sum == target) {
                     return sum;
-                } else if (sum < target) {
+                }
+
+                if (Math.abs(sum - target) < Math.abs(minSum - target)) {
+                    minSum = sum;
+                }
+                if (sum < target) {
                     beg++;
                 } else {
                     end--;
                 }
             }
         }
-        return closest;
+        return minSum;
     }
 }
