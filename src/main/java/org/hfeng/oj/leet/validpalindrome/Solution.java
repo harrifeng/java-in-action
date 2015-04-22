bb169b05cae384c230293398994ea4dce3b445b4
Haoran Feng <haoran.feng@sap.com>
2015-04-22 17:40:15 +0800

    my solution but is not very good compare to replaceAll("^[a-zA-Z0-9]", "").toLowerCase()

--- src/main/java/org/hfeng/oj/leet/validpalindrome/Solution.java
+++ src/main/java/org/hfeng/oj/leet/validpalindrome/Solution.java
@@ -2,16 +2,26 @@ package org.hfeng.oj.leet.validpalindrome;
 
 public class Solution {
     public boolean isPalindrome(String s) {
-        if (s == null || s.length() == 0) {
+        if (s.length() < 2) {
             return true;
         }
+        int beg = 0;
+        int end = s.length() - 1;
 
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        while (beg < end) {
+            while (beg < s.length() && !Character.isLetterOrDigit(s.charAt(beg))) {
+                beg++;
+            }
+            while (end > 0 && !Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            }
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+            if (beg < end &&
+                Character.toLowerCase(s.charAt(beg)) != Character.toLowerCase(s.charAt(end))) {
                 return false;
             }
+            beg++;
+            end--;
         }
         return true;
     }
