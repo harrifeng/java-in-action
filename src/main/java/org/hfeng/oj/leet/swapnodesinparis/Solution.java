88a9f55fa553c766f879ae4dcb9694737e0dc8cf
Haoran Feng <haoran.feng@sap.com>
2015-04-22 14:54:24 +0800

    A Better solution is to use recursive method It is the same with *Reverse Nodes in k-Group*

--- src/main/java/org/hfeng/oj/leet/swapnodesinparis/Solution.java
+++ src/main/java/org/hfeng/oj/leet/swapnodesinparis/Solution.java
@@ -1,29 +1,18 @@
 package org.hfeng.oj.leet.swapnodesinparis;
 
-import org.hfeng.oj.leet.util.ListNode;
+import org.hfeng.oj.leet.util.*;
 
-class Solution {
+public class Solution {
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode tmp = dummy;
-        ListNode before = head;
-        ListNode after = head.next;
 
-        while (after != null) {
-            head = after.next;
-            after.next = before;
-            before.next = head;
-            dummy.next = after;
+        ListNode newNext = head.next.next;
+        ListNode newFront = head.next;
 
-            dummy = before;
-            before = head;
-            after = (head == null) ? null : head.next;
-        }
-
-        return tmp.next;
+        head.next.next = head;
+        head.next = swapPairs(newNext);
+        return newFront;
     }
 }
