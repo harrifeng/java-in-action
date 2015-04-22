b105263dcc73db2eb9feeb7a6c311179a1cdb4c7
Haoran Feng <haoran.feng@sap.com>
2015-04-21 18:17:23 -0700

    my solution

--- src/main/java/org/hfeng/oj/leet/simplifypath/Solution.java
+++ src/main/java/org/hfeng/oj/leet/simplifypath/Solution.java
@@ -1,28 +1,28 @@
 package org.hfeng.oj.leet.simplifypath;
 
-import org.hfeng.book.tij4.util.Stack;
+import java.util.Stack;
 
 public class Solution {
     public String simplifyPath(String path) {
-        Stack<String> s = new Stack<String>();
-        String[] split = path.split("/");
-        for (String a : split) {
-            if (!a.equals(".") && !a.isEmpty()) {
-                if (a.equals("..")) {
-                    if (!s.empty()) {
-                        s.pop();
+        Stack<String> stack = new Stack<String>();
+        String[] parts = path.split("/");
+        for (int i = 0; i< parts.length; i++) {
+            if (!parts[i].isEmpty() &&!parts[i].equals(".")) {
+                if (parts[i].equals((".."))) {
+                    if (!stack.empty()) {
+                        stack.pop();
                     }
                 } else {
-                    s.push(a);
+                    stack.push(parts[i]);
                 }
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        while (!s.empty()) {
-            sb.insert(0, s.pop());
-            sb.insert(0, "/");
+        StringBuilder ret = new StringBuilder();
+        while (!stack.empty()) {
+            ret.insert(0, stack.pop());
+            ret.insert(0, "/");
         }
-        return sb.length() == 0 ? "/" : sb.toString();
+        return ret.length() == 0 ? "/" : ret.toString();
     }
 }
