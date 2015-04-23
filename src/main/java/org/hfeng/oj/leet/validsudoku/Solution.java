402479e2b7b604130350c87a7287bd5137842982
Haoran Feng <haoran.feng@sap.com>
2015-04-23 18:16:16 +0800

    my solution

--- src/main/java/org/hfeng/oj/leet/validsudoku/Solution.java
+++ src/main/java/org/hfeng/oj/leet/validsudoku/Solution.java
@@ -4,28 +4,32 @@ import java.util.*;
 
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        List<boolean[]> lineList = new ArrayList<boolean[]>();
-        List<boolean[]> columnList = new ArrayList<boolean[]>();
-        List<boolean[]> squareList = new ArrayList<boolean[]>();
+
+        // use boolean[] is OK, as boolean[] is object, while boolean is not
+        List<boolean[]> row = new ArrayList<boolean[]>();
+        List<boolean[]> column = new ArrayList<boolean[]>();
+        List<boolean[]> small = new ArrayList<boolean[]>();
 
         for (int i = 0; i < 9; i++) {
-            lineList.add(new boolean[9]);
-            columnList.add(new boolean[9]);
-            squareList.add(new boolean[9]);
+            row.add(new boolean[9]);
+            column.add(new boolean[9]);
+            small.add(new boolean[9]);
         }
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if(board[i][j] == '.') {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
                     continue;
                 }
-                int num = board[i][j] - '1';
-                if(lineList.get(i)[num] || columnList.get(j)[num] || squareList.get(i / 3 * 3 + j/ 3)[num]) {
+                //boolean[9] is actually boolean[0] to boolean[8]
+                int cur = board[i][j] - '1';
+                if (row.get(i)[cur] || column.get(j)[cur] || small.get(i / 3 * 3 + j / 3)[cur]) {
                     return false;
+                } else {
+                    row.get(i)[cur] = true;
+                    column.get(j)[cur] = true;
+                    small.get(i / 3 * 3 + j / 3)[cur] = true;
                 }
-                lineList.get(i)[num] = true;
-                columnList.get(j)[num] = true;
-                squareList.get(i / 3 * 3 + j / 3)[num] = true;
             }
         }
         return true;
