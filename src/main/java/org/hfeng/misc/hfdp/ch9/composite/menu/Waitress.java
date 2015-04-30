8a9572b858d5deefdd91e837fefa1dd300da1528
Haoran Feng <haoran.feng@sap.com>
2015-04-29 22:00:15 -0700

    composite pattern

---
+++ src/main/java/org/hfeng/misc/hfdp/ch9/composite/menu/Waitress.java
@@ -0,0 +1,13 @@
+package org.hfeng.misc.hfdp.ch9.composite.menu;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
