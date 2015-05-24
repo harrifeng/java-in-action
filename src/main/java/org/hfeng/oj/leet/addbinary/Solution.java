5fa15bbd6d8084a359c0d719bff3b823d838096b
harri feng <harrifeng@gmail.com>
2015-05-24 20:55:07 +0800

    notice i and j

--- src/main/java/org/hfeng/oj/leet/addbinary/Solution.java
+++ src/main/java/org/hfeng/oj/leet/addbinary/Solution.java
@@ -1,18 +1,25 @@
 package org.hfeng.oj.leet.addbinary;
 public class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
         int adv = 0;
-        for (int i = 0; i < a.length() || i < b.length(); i++) {
-            char cha = a.length() > i ? a.charAt(a.length() - i - 1) : '0';
-            char chb = b.length() > i ? b.charAt(b.length() - i - 1) : '0';
-            int sum = adv + (cha - '0') + (chb - '0');
-            sb.insert(0, (sum % 2)) ;
+        StringBuilder ret = new StringBuilder();
+
+        for (int i= a.length() - 1, j= b.length() - 1; i >= 0 || j>= 0; ) {
+            int sum = adv;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
             adv = sum / 2;
+            ret.insert(0, sum % 2);
         }
-        if (adv != 0) {
-            sb.insert(0, '1');
+        if (adv == 1) {
+            ret.insert(0, 1);
         }
-        return sb.toString();
+        return ret.toString();
     }
 }
